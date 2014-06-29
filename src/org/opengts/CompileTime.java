3531103e0ca324a2381451b1b2f70a800d568e64
Haris Peco <snpe@snpe.rs>
2014-06-29 17:57:39 +0200

    Initial import

---
+++ src/org/opengts/CompileTime.java
@@ -0,0 +1,10 @@
+package org.opengts;
+public class CompileTime
+{
+    // 2014/06/28 16:47:39 CEST
+    public static final long   COMPILE_TIMESTAMP    = 1403966859L;
+    public static final String COMPILE_DATETIME     = "2014/06/28 16:47:39 CEST";
+    public static final String SERVICE_ACCOUNT_ID   = "opengts";
+    public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
+    public static final String SERVICE_ACCOUNT_KEY  = "";
+}
